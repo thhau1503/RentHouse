029c0a03f2716133f6fe0249caec2b7e37f5386c
thhau1503 <thanhhaudeb204@gmail.com>
2024-11-24 16:37:44 +0700

    update cors config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes/AppRoutes";
-//import AuthProvider from "./contexts/AuthContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AuthProvider from "./contexts/AuthContext";
 import "./assets/styles/globals.css";
-import "./index.css"; // Nhập tệp CSS của Tailwind
+import "./index.css"; 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
